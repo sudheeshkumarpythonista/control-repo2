c9c9867c0a923bb0bb39a64138b2212ae65ad798
sudheeshkumarpythonista <81633927+sudheeshkumarpythonista@users.noreply.github.com>
2021-07-15 20:16:17 +0530

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,6 +1,18 @@
 node default {
+  file { 'root/README':
+      ensure => file,
+      content => 'This is a readme',
+      owner => 'root'
    }
 node 'master.puppet.vm' {
   include role:master_server
   
   }
+  
+node /^web/ {
+    include role::role_appserver
+    }
+    
+node /^db/ {
+    include role::db_appserver
+    }
